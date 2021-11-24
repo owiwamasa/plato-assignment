e332333b33944cae9d6a52b7e69b8b6d3b4265a8
Owen Iwamasa <owiwamasa@gmail.com>
2021-11-24 14:59:09 -0800

    refactored

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const utils = require('./utils')
+const {getCsvData, combineMarksTestsAndCourses, checkCourseWeights, studentToJson} = require('./utils')
 
 const csvFiles = process.argv.slice(2);
 
 function csvToJson(csvFiles){
-    const { courses, students, tests, marks } = utils.getCsvData(csvFiles)
-    const combinedMarks = utils.combineMarksTestsAndCourses(marks, tests, courses)
+    const { courses, students, tests, marks } = getCsvData(csvFiles)
+    const combinedMarks = combineMarksTestsAndCourses(marks, tests, courses)
     const jsonOutput = { "students": [] }
 
-    if (!utils.checkCourseWeights(tests)) return { "error": "Invalid course weights" }
+    if (!checkCourseWeights(tests)) return { "error": "Invalid course weights" }
 
     for (let key in students) {
-        let student = utils.studentToJson(students[key], combinedMarks)
+        let student = studentToJson(students[key], combinedMarks)
         jsonOutput["students"].push(student)
     }
     return JSON.stringify(jsonOutput)
